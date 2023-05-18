325441800c0117ddb3394eeed10bb276aba5f89d
RauloGti <raulogtis@gmail.com>
2023-05-18 16:26:51 -0300

    login html, formcontrol,buttons login, register

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,13 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
 
-  constructor() { }
+export class LoginPage implements OnInit {
+//formulario reactivo con validaciones para usar la importacion del metodo FormBuilder
+  form = this.formBuilder.group({
+    email:['',[Validators.email,Validators.required]],
+    password: ['',[Validators.required]],
+  })
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
   }
