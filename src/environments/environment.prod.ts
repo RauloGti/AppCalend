500c5d3190c6b9b27a123f27beefbf82d0cde406
RauloGti <raulogtis@gmail.com>
2023-05-20 01:41:32 -0300

    se realizo la instalacion e integracion de @angular/fire y se realizo el primer deploy de la app

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'calendaap-66dde',
+    appId: '1:428884575058:web:394621aa0f28516d5df08b',
+    storageBucket: 'calendaap-66dde.appspot.com',
+    apiKey: 'AIzaSyDPaox9UQzF6uqJyvZKR2N-9awvtAoI7hE',
+    authDomain: 'calendaap-66dde.firebaseapp.com',
+    messagingSenderId: '428884575058',
+    measurementId: 'G-B9JHVF4ZGD',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'calendaap-66dde',
+    appId: '1:428884575058:web:394621aa0f28516d5df08b',
+    storageBucket: 'calendaap-66dde.appspot.com',
+    apiKey: 'AIzaSyDPaox9UQzF6uqJyvZKR2N-9awvtAoI7hE',
+    authDomain: 'calendaap-66dde.firebaseapp.com',
+    messagingSenderId: '428884575058',
+    measurementId: 'G-B9JHVF4ZGD',
+  },
   production: false
 };
 
