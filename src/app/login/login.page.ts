0893e0b494d6925cc06fd5a8da56dcedb14aac80
RauloGti <raulogtis@gmail.com>
2023-05-20 13:31:24 -0300

    realizamos formulario de registro.validacion Auth ingreso con usuario y contraseña y como resultado el ingreso al home de la app

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -13,20 +15,46 @@ export class LoginPage implements OnInit {
     email:['',[Validators.email,Validators.required]],
     password: ['',[Validators.required]],
   })
+  auth: any;
 
   constructor(
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private authService : AuthService,
+    private router : Router
   ) { }
 
   ngOnInit() {
   }
   //funcion login para conectar "logica" y validar los datos
-  login(){
-    if(this.form.valid){
-      const{email, password} = this.form.getRawValue();
-      console.log(email,password);
+  login() {
+    if (this.form.valid) {
+      const { email, password } = this.form.getRawValue();
+      if (email && password) { // Verificar si email y password no son null
+        this.auth.register(email, password)
+          .then(() => {
+            this.router.navigate(['/home']);
+          })
+          .catch((error: any) => {
+            console.error(error);
+          });
+      }
     } else {
       this.form.markAllAsTouched();
     }
   }
+  /*
+  login(){
+    if(this.form.valid){
+      const{email, password} = this.form.getRawValue();
+      this.auth.register(email, password)
+          .then(() => {
+            this.router.navigate(['/home']);
+          })
+          .catch(error => {
+            console.error(error);
+          });
+    } else {
+      this.form.markAllAsTouched();
+    }
+  }*/
 }
--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from './../services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -10,25 +12,52 @@ export class RegisterPage implements OnInit {
 
   //formulario reactivo con validaciones para usar la importacion del metodo FormBuilder
   form = this.formBuilder.group({
-    email:['',[Validators.email,Validators.required]],
+    email:['', [Validators.email,Validators.required]],
     password: ['',[Validators.required]],
     confirmPassword: ['',[Validators.required]],
   })
 
   constructor(
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private auth: AuthService,
+    private router : Router
   ) { }
 
   ngOnInit() {
   }
   //funcion login para conectar "logica" y validar los datos
-  register(){
-    if(this.form.valid){
-      const{email, password, confirmPassword} = this.form.getRawValue();
-      console.log(email,password);
+  register() {
+    if (this.form.valid) {
+      const { email, password } = this.form.getRawValue();
+      if (email && password) { // Verificar si email y password no son null
+        this.auth.register(email, password)
+          .then(() => {
+            this.router.navigate(['/home']);
+          })
+          .catch(error => {
+            console.error(error);
+          });
+      }
     } else {
       this.form.markAllAsTouched();
     }
   }
-
+} 
+  
+  /*register(){
+    if(this.form.valid){
+      const { email, password } = this.form.getRawValue();
+      this.auth.register(email, password)
+      .then(() => {
+        this.router.navigate(['/home']);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+    } else {
+        this.form.markAllAsTouched();
+    }
+  }
 }
+*/
+
