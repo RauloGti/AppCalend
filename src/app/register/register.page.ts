e0befb1863f431afca0a486f2dfe474673e23cd6
MatiasIgnacioSoria <matiasignaciosoria@gmail.com>
2023-06-13 18:16:30 -0300

    se agregaron cosas

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from './../services/auth.service';
 
 import { ToastController } from '@ionic/angular';
@@ -14,11 +14,8 @@ import { ToastController } from '@ionic/angular';
 export class RegisterPage implements OnInit {
 
   //formulario reactivo con validaciones para usar la importacion del metodo FormBuilder
-  form = this.formBuilder.group({
-    email:['', [Validators.email,Validators.required]],
-    password: ['',[Validators.required]],
-    confirmPassword: ['',[Validators.required]],
-  })
+  form:FormGroup;
+  passwordsMatch: boolean = true;
 
   constructor(
     private formBuilder: FormBuilder,
@@ -27,8 +24,14 @@ export class RegisterPage implements OnInit {
     private toastController: ToastController,
     
     
-  ) { }
-
+  ) {
+    this.form = this.formBuilder.group({
+      email:['', [Validators.required,Validators.required, Validators.pattern('[^@]+@[^@]+\.[a-zA-Z]{2,6}')]],
+      password: ['',[Validators.required,Validators.minLength(6)]],
+      confirmPassword: ['',[Validators.required]],
+    }, { validator: this.passwordMatchValidator.bind(this) });
+   }
+   
   async mostrarError(mensaje: string) {
     const toast = await this.toastController.create({
       message: mensaje,
@@ -37,38 +40,42 @@ export class RegisterPage implements OnInit {
     });
     toast.present();
   }
-
+  passwordMatchValidator(formGroup: FormGroup) {
+    const passwordControl = formGroup.get('password');
+    const confirmPasswordControl = formGroup.get('confirmPassword');
+  
+    if (passwordControl?.value !== confirmPasswordControl?.value) {
+      confirmPasswordControl?.setErrors({ passwordMismatch: true });
+    } else {
+      confirmPasswordControl?.setErrors(null);
+    }
+  }
   ngOnInit() {
   }
 
     //funcion login para conectar "logica" y validar los datos
     register() {
-    if (this.form.valid) {
-      const { email, password } = this.form.getRawValue();
-      //validar que pass y repass sean iguales
-      if (email && password  ) { // Verificar si email y password no son null
-        this.auth.register(email, password, )
-        
-          .then(() => {
-            this.router.navigate(['/login']);
-          })
-          
-          .catch(error => {
-            console.error(error);
-            if (error.code === 'auth/email-already-in-use') {
-              this.mostrarError('El correo electrónico ya está en uso.');
-            } else if (error.code === 'auth/weak-password') {
-              this.mostrarError('La contraseña es débil. Debe tener al menos 6 caracteres.');
-            } else  {
-              this.mostrarError('Ocurrió un error al registrar el usuario.');
-            }
-          
-          });
+      if (this.form.valid) {
+        const { email, password } = this.form.getRawValue();
+        //validar que pass y repass sean iguales
+        if (email && password) { // Verificar si email y password no son null
+          this.auth.register(email, password)
+            .then(() => {
+              this.router.navigate(['/login']);
+            })
+            .catch(error => {
+              console.error(error);
+              if (error.code === 'auth/weak-password') {
+                this.mostrarError('La contraseña es débil. Debe tener al menos 6 caracteres.');
+              } else if (error.code === 'auth/email-already-in-use') {
+                this.mostrarError('El correo electrónico ya está en uso.');
+              } else {
+                this.mostrarError('Ocurrió un error al registrar el usuario.');
+              }
+            });
+        }
       }
-    } else {
-      this.form.markAllAsTouched();
     }
-  }
 } 
   
   /*register(){
