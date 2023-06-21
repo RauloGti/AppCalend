e6f4c03f25bf36393753d7540e30b990589e2ed7
RauloGti <raulogtis@gmail.com>
2023-06-21 19:36:24 -0300

    se agrego la carpeta android al gitignore

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.tuapp.calendAap',
   appName: 'CalendAap',
   webDir: 'www',
   server: {
